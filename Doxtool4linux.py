f7eaf37e96cffe4550c5510d713393f6dde259f0
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-10 16:48:45 +0500

    Doxtool4linux.py

---
+++ Doxtool4linux.py
@@ -0,0 +1,219 @@
+import os
+
+
+print('''
+
+If you dont know some of answers then type N/A
+
+''')
+#basic info
+fname = input("Enter the first name of the victim: ")
+
+mname = input("Enter the Middle name of the victim: ")
+
+lname = input("Enter the last name of the victim: ")
+
+Emails = input("Enter Email(s) of the victim: ")
+
+
+ssn = str(input("Enter ssn of the victim: "))
+
+age = input("Enter the age of the victim: ")
+
+birthdate = input("Enter birthdate of the victim: ")
+
+
+
+numbs = input("Enter Phone Number(s) of the victim: ")
+
+gender = input("Enter the gender of the victim: ")
+
+facepic = input("Enter the link of his/her face picture: ")
+
+#location
+
+addy = input("Enter the address of the victim: ")
+
+zipcode = input("Enter the zipcode of the victim: ")
+
+city = input("Enter the city name of the victim: ")
+
+state = input("Enter the state/province/region name of the victim: ")
+
+country = input("Enter the country name of the victim: ")
+
+cordinates = input("Enter the cordinates of the victim: ")
+
+#school details
+
+schoolname = input("Enter the name of school he went: ")
+
+schooladdy = input("Enter the school address: ")
+
+#job details
+
+job = input("whats the job of the victim: ")
+
+jobdetails = input("Enter some details u know about his job: ")
+
+
+jobaddy = input("Enter the address of his job place: ")
+
+extrajobinfo = input("Add some extra info about his/her job: ")
+
+#relatives
+
+
+fathername = input("Enter the full name of his/her father: ")
+
+mothername = input("Enter the full name of his/her mother: ")
+
+
+brothername = input("Enter the full name of his/her brother: ")
+
+sistername = input("Enter the full name of his/her sister: ")
+
+friends = input("Enter the fullnames of his/her friends: ")
+
+partner = input("Enter the name of victim\'s WIFE/HUSBAND/GIRLFRIEND/BOYFRIEND/FIANCE: ")
+
+orelatives = input("Enter the name of some other relatives you know of his/her: ")
+
+#IP and ports
+
+IP = input("Enter the IP(s) of the victim: ")
+
+port = input("Enter the port(s) of the victim: ")
+
+#social medias
+
+snap = input("Enter the snapchat profile link of the victim: ")
+
+insta = input("Enter the Instagram profile link of the Victim: ")
+
+fb = input("Enter the Facebook profile link of the victim: ")
+
+twitter = input("Enter the twitter profile link of the victim: ")
+
+youtube = input("Enter the YouTube channel link of the victim: ")
+
+skype = input("Enter the skype profile link of the victim: ")
+
+osm = input("Enter the other social medias you know of him/her: ")
+
+#other informations
+
+oinfo = input("Enter some other infos you know about him: ")
+print("")
+print("")
+os.system('clear')
+
+print("[!]Basic Info")
+print("")
+print("")
+print("FirstName: "+fname)
+
+print("MiddleName: "+mname)
+
+print("LastName: "+lname)
+
+print("Emails(s): "+Emails)
+
+print("SSN: "+ssn)
+
+print("Age: "+age)
+
+print("birthdate: "+birthdate)
+
+print("Phone Number(s): "+numbs)
+
+
+print("Gender: "+gender)
+
+print("Face Picture: "+facepic)
+
+print("")
+print("")
+print("[!]location")
+print("")
+print("")
+
+print("Address: "+addy)
+print("City: "+city)
+print("State: "+state)
+print("ZipCode: "+zipcode)
+print("Country: "+country)
+print("Cordinates: "+cordinates)
+
+
+print("")
+print("")
+
+
+print("[!]School information")
+print("")
+print("")
+
+print("School Name: "+schoolname)
+print("School address: "+schooladdy)
+print("")
+print("")
+print("[!]Job Details")
+
+print("")
+print("")
+
+print("Job: "+job)
+print("Job Details: "+jobdetails)
+print("Job address: "+jobaddy)
+print("Extra Informations about Job: "+extrajobinfo)
+print("")
+print("")
+print("[!]Relatives")
+
+print("")
+print("")
+
+print("Father\'s Name: "+fathername)
+print("Mother\'s Name: "+mothername)
+print("Brother\'s Name: "+brothername)
+print("Sister\'s Name: "+sistername)
+print("Friends: "+friends)
+print("HUSBAND/WIFE/GIRLFRIEND/BOYFRIEND/FIANCE: "+partner)
+print("Other Relatives: "+orelatives)
+print("")
+print("")
+print("[!]IP AND PORTS")
+
+print("")
+print("")
+
+print("IP(s): "+IP)
+print("Port(s): "+port)
+print("")
+print("")
+print("[!]Social Medias")
+print("")
+print("")
+
+print("Snapchat: "+snap)
+print("Instagram: "+insta)
+print("Facebook: "+fb)
+print("Twitter: "+twitter)
+print("YouTube: "+youtube)
+print("Skype: "+skype)
+print("Other social medias: "+osm)
+print("")
+print("")
+print("[!]Other Informations")
+
+print("")
+print("")
+
+print("Other Info: "+oinfo)
+
+print("")
+
+print("")
+
+print("Copyrights V1MoDzZz - 2020")
